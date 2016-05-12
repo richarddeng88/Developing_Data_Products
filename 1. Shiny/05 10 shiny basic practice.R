c0bd9151a7326316f0403032b2292fae3853477a
Richard <richarddeng88@gmail.com>
2016-05-12 00:19:16 -0400

    05 11

---
+++ 1. Shiny/05 10 shiny basic practice.R
@@ -0,0 +1,51 @@
+library(shiny)
+ui <- fluidPage(
+    
+    titlePanel(h1("Richard's shiny parctice 05/11/2016", align="center")),
+    
+    sidebarLayout(
+    sidebarPanel(  
+    
+    h3("Choose the dist type"),
+    actionButton("norm","Normal distribution"),
+    actionButton("unif","Uniform"),
+    
+    h3("choose and click update"),
+    sliderInput("num", "choose a number", min=1, max=10000, value=2000, step = 5),
+    sliderInput("bin","choose the bin", min=20,max=100, value=50,step=2),
+    actionButton("go", "update"),
+    
+    actionButton("click","click me")
+    ),
+    
+    mainPanel(
+        a(href="http://www.github.com/richarddeng88/citibike","my github code link"),br(),
+        em("this is my practice for interface style"),br(),
+        strong("this is strong case!"),br(),
+        code("This is a Shiny app."),br(),
+        p("This is a",tags$strong("Shiny"),"app."),
+        img(height = 100,width = 280,src = "http://www.rstudio.com/images/RStudio.2x.png"),hr(),
+        
+        plotOutput("type"),
+        plotOutput("hist")
+    
+    )
+    )
+)
+
+server <- function(input, output){
+    
+    rv <- reactiveValues(data=rnorm(100)) #
+    observeEvent(input$norm, {rv$data <- rnorm(10000)})
+    observeEvent(input$unif, {rv$data <- runif(10000)})
+    output$type <- renderPlot({hist(rv$data, breaks=28)})
+    
+    
+    data <- eventReactive(input$go ,{hist(rnorm(input$num),breaks=input$bin)})
+    output$hist <- renderPlot({data()})
+    click <- observeEvent(input$click, {print(as.numeric(input$click))})
+    
+}
+
+
+shinyApp(ui=ui, server=server)
---
+++ 1. Shiny/05 11 shiny layout page design.R
@@ -0,0 +1,128 @@
+library(shiny);library(shinydashboard)
+
+ui <- dashboardPage(skin = "purple",
+    
+    dashboardHeader(title= "Citi Bike Ridership",
+                    
+                    dropdownMenu(type = "messages",
+                                 messageItem(
+                                     from = "Sales Dept",
+                                     message = "Sales are steady this month."
+                                 ),
+                                 messageItem(
+                                     from = "New User",
+                                     message = "How do I register?",
+                                     icon = icon("question"),
+                                     time = "13:45"
+                                 ),
+                                 messageItem(
+                                     from = "Support",
+                                     message = "The new server is ready.",
+                                     icon = icon("life-ring"),
+                                     time = "2014-12-01"
+                                 )
+                    ),
+                    
+                    
+                    dropdownMenu(type = "notifications",
+                                 notificationItem(
+                                     text = "5 new users today",
+                                     icon("users")
+                                 ),
+                                 notificationItem(
+                                     text = "12 items delivered",
+                                     icon("truck"),
+                                     status = "success"
+                                 ),
+                                 notificationItem(
+                                     text = "Server load at 86%",
+                                     icon = icon("exclamation-triangle"),
+                                     status = "warning"
+                                 )
+                    ),
+                    
+                    
+                    dropdownMenu(type = "tasks", badgeStatus = "success",
+                                 taskItem(value = 90, color = "green",
+                                          "Documentation"
+                                 ),
+                                 taskItem(value = 17, color = "aqua",
+                                          "Project X"
+                                 ),
+                                 taskItem(value = 75, color = "yellow",
+                                          "Server deployment"
+                                 ),
+                                 taskItem(value = 80, color = "red",
+                                          "Overall project"
+                                 )
+                    )
+                    
+                    ),
+   
+    
+    sidebar <- dashboardSidebar(
+        sidebarMenu(
+            menuItem("Exploratory Analysis", tabName = "eda", icon = icon("dashboard")),
+            menuItem("Modeling", tabName = "modeling", icon = icon("th"),badgeLabel = "new", badgeColor = "green"),
+            menuItem("Geo",tabName = "geo",icon=icon("calendar")),
+            sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
+                              label = "Search..."),
+            menuItem("Source code", icon = icon("file-code-o"), 
+                     href = "https://github.com/richarddeng88/citibike")
+        )
+    ),
+    
+    
+    
+    
+    body <- dashboardBody(
+        tabItems(
+            # First tab content
+            tabItem(tabName = "eda",
+                    fluidRow(
+                        box(plotOutput("plot1", height = 250)),
+                        
+                        box(
+                            title = "Controls",
+                            sliderInput("slider", "Number of observations:", 1, 100, 50)
+                        )
+                    )
+            ),
+            
+            # Second tab content
+            tabItem(tabName = "modeling",
+                    h2("Widgets tab content")
+            ),
+            
+            
+            # Third tab content
+            tabItem(tabName = "geo",
+                    h2("Geographic tab content")
+            )
+        )
+        
+    
+    )
+    
+    
+    
+    
+    
+    
+)
+
+server <- function(input, output) { 
+    
+        set.seed(122)
+        histdata <- rnorm(500)
+        
+        output$plot1 <- renderPlot({
+            data <- histdata[seq_len(input$slider)]
+            hist(data)
+        })
+        
+    }
+
+
+
+shinyApp(ui=ui, server=server)
